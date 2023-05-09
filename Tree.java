2a4ceb71bdf47970d48edc32bef669ccbd0d390d
Elizabeth <tjeudy.e@gmail.com>
2023-05-08 20:19:14 -0400

    first commit

---
+++ Tree.java
@@ -0,0 +1,13 @@
+public class Tree {
+  public static void main(String[] args) {
+    // introducing ourselves and printing a tree to screen
+    System.out.println("");
+    System.out.println("I'm going to plant a tree today!");
+       System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");  
+  }
+    
+  }
